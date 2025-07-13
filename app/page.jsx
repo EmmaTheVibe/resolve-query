f76fbf1003d0a058bda4e7f9aa2aae9e5dc3e18c
EmmaTheVibe <peteremmanuel316@gmail.com>
2025-07-13 16:15:20 +0100

    push 1

---
+++ app/page.jsx
@@ -0,0 +1,9 @@
+import CandidateAssignmentSystem from "./_components/CandidateAssignmentSystem";
+
+export default function Page() {
+  return (
+    <div>
+      <CandidateAssignmentSystem />
+    </div>
+  );
+}
